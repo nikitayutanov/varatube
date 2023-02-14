4d8b655e54f16e42fe752589bd43e2386794ca0d
Nikita Yutanov <nikitayutanov@gmail.com>
2023-02-14 20:33:09 +0300

    Add home page links

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,25 @@
+import { buttonStyles } from '@gear-js/ui';
+import clsx from 'clsx';
 import { useSubscriptions } from 'hooks';
+import { Link } from 'react-router-dom';
+import styles from './Home.module.scss';
 
 function Home() {
   const { subscriptionsState } = useSubscriptions();
-  console.log('subscriptionsState: ', subscriptionsState);
+  console.log(subscriptionsState);
 
-  return <>main</>;
+  return (
+    <>
+      <Link
+        to="subscription"
+        className={clsx(buttonStyles.button, buttonStyles.large, buttonStyles.primary, styles.link)}>
+        My Subscription
+      </Link>
+      <Link to="videos" className={clsx(buttonStyles.button, buttonStyles.large, buttonStyles.secondary)}>
+        Videos
+      </Link>
+    </>
+  );
 }
 
 export { Home };
